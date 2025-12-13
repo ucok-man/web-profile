fad5f547112a02718224420f116de540fde701ed
ucok-man <ucokkocu411@gmail.com>
2025-12-13 21:42:42 +0700

    feat: add swipe icon

--- app/(alvin odhi)/_components/section-projects/index.tsx
+++ app/(alvin odhi)/_components/section-projects/index.tsx
@@ -3,6 +3,7 @@ import { Grid, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Project, PROJECT_LIST } from "@/components/datas/project";
+import Image from "next/image";
 import "swiper/css";
 import "swiper/css/grid";
 import "swiper/css/navigation";
@@ -20,7 +21,16 @@ export default function SectionProjects({ onRef }: Props) {
       className="py-20 sm:py-32 opacity-0"
     >
       <div className="space-y-12 sm:space-y-16">
-        <Heading2>My Projects</Heading2>
+        <div className="flex items-center gap-2">
+          <Heading2>My Projects</Heading2>
+          <Image
+            alt="Swipe"
+            src={"/swipe.png"}
+            width={40}
+            height={40}
+            className="relative -top-1"
+          />
+        </div>
 
         <Swiper
           modules={[Navigation, Grid, Pagination]}
@@ -38,7 +48,7 @@ export default function SectionProjects({ onRef }: Props) {
           }}
         >
           {PROJECT_LIST.map((item) => (
-            <SwiperSlide key={item.name}>
+            <SwiperSlide key={item.name} className="h-fit!">
               <Project.Card item={item} />
             </SwiperSlide>
           ))}
